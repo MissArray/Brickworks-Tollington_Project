fc4612f8558dd5125718c20208460fae85cc4664
MissArray <33687551+MissArray@users.noreply.github.com>
2018-10-28 12:15:57 +0000

    Added images to Inspirations page

--- client/src/components/pages/inspirations.js
+++ client/src/components/pages/inspirations.js
@@ -2,6 +2,10 @@ import React from 'react';
 import fbLogo from '../../../public/images/facebook-logo.svg';
 import twitterLogo from '../../../public/images/twitter-logo.svg';
 import emailSymbol from '../../../public/images/email-symbol.svg';
+import inspBricks from  '../../../public/images/Brickworks-sketched-ideas.jpg';
+import inspBricks1 from '../../../public/images/Brickworks-sketched-ideas-1.jpg';
+import inspBricks2 from '../../../public/images/Brickworks-sketched-ideas-2.jpg';
+
 
 class Inspiration extends React.Component {
     render(){
@@ -36,7 +40,16 @@ class Inspiration extends React.Component {
                     Even before moving into the building we began to foster local relationships and build a sense of power, purpose and capability, so that local people feel that they can make a difference in their neighbourhood. Together with Islington Council we are now exploring how we can work more closely to involve local people in decisions that inpact where they live. We've also trained around 30 local Community Organisers, including local people, volunteers and community-sector workers, as well as staff within the Homes and Communities team in Islington Council.
                     <br/>
                     <br/>
+                        <div className='img-container'>
+                        <img id='insp-Brickworks' src={inspBricks} className='single-event-img' alt='sketched ideas for Brickworks' />
+                        </div>                
+                    <br/>
                     We are now supporting local people to develop their own social actions, to increase the sense of power and forge relationships with those providing public services to explore how local people can be more involved in local decision making and how they might better influence service provision.
+                    <br/>
+                    <br/>
+                    <div className='img-container'>
+                        <img id='insp-Brickworks-2' className='single-event-img' src={inspBricks2} alt='sketched ideas for Brickworks' />
+                        </div> 
                 </p>
                 </li>
 
@@ -47,6 +60,10 @@ class Inspiration extends React.Component {
                 <p className='li-content'>
                 Islington Council has chosen the ward of Tollington to explore and develop a 'Locality' initiative that aims to develop, define, and test the 'community' aspect of the Homes and Communities offer. A team of 11 staff from Homes and Communities and key local partner are engaging and collaborating to ensure that Homes and Communities are connected to and knowledgeable about the communities that they work in.
                 <br/>
+                <br/>
+                <div className='img-container'>
+                        <img id='insp-Brickworks-1' className='single-event-img' src={inspBricks1} alt='sketched ideas for Brickworks' />
+                        </div> 
                 <br/>
                 The first phase of 'Locality' has focused on Insights and Data Gathering, which has revealed some interesting facts and statistics and this combined with the engagement of local organisations, businesses and residents has identified some key areas of concerns linked to poverty, isolation and health and well-being, which are now the key focus for collective social action with Brickworks being developed, supported and promoted as the heart of the community.
                 </p>
