181ee53c0ada645bc9eca4e07500544119ced59a
MissArray <33687551+MissArray@users.noreply.github.com>
2018-10-28 14:15:39 +0000

    Readjusted footer

--- client/src/components/mainPageComponents/footer.js
+++ client/src/components/mainPageComponents/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer>
-   &nbsp; &nbsp; &nbsp; &nbsp; &copy; Brickworks Social Action Hub 2018  &nbsp;  &mdash; &nbsp;   Web app by Founders & Coders
+   &copy; Brickworks Social Action Hub 2018
   </footer>
 
 )
